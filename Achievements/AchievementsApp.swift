b3a673bf57956c465d4b726a21a9e36bf95cb661
Jakob Woodard <jakobwoodard@icloud.com>
2024-05-03 14:36:19 -0400

    Initial Commit

---
+++ Achievements/AchievementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AchievementsApp.swift
+//  Achievements
+//
+//  Created by Jakob Woodard on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AchievementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Achievements/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Achievements
+//
+//  Created by Jakob Woodard on 5/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
